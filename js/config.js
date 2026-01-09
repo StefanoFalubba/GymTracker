b501bf9fd6fbde84370aaf04aee3f5b217d9e645
StefanoF <stefano.falubba@gmail.com>
2026-01-09 12:25:03 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // Get them from: https://app.supabase.com/project/_/settings/api
 
 export const SUPABASE_CONFIG = {
-    url: 'https://lymwbqttehyxdroeeqpi.supabase.co/', // e.g., 'https://xxxxx.supabase.co'
+    url: 'https://lymwbqttehyxdroeeqpi.supabase.co', // e.g., 'https://xxxxx.supabase.co'
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5bXdicXR0ZWh5eGRyb2VlcXBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5NDUzNzIsImV4cCI6MjA4MzUyMTM3Mn0.VgbG-ZXCJPM56GT55U0QTHZsfx-7FufDWaNRQv0M6fs' // Your public anon key
 };
 
